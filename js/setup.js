7648409e039aec0b3603bdc94c9b052a01b7cb82
arobbi7 <arobbins@azavea.com>
2017-06-04 15:49:31 -0400

    Use createLayer instead of Carto SQL API directly

--- js/setup.js
+++ js/setup.js
@@ -1,78 +1,29 @@
-var app = {
-  apikey: "83132e6826514aaa5f3c2fd1ff6b0c2be6616363",
-  map: L.map('map', { center: [30.941525, -7.214665], zoom: 3 }),
-  geojsonClient: new cartodb.SQL({ user: 'arobbins', format: 'geojson' }),
-  drawnItems: new L.FeatureGroup()
-};
+// Leaflet map setup
+var map = L.map('map', {
+  center: [30.941525, -7.214665],
+  zoom: 3
+});
 
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   subdomains: 'abcd',
   minZoom: 0,
   maxZoom: 20,
   ext: 'png'
-}).addTo(app.map);
+}).addTo(map);
 
-// The initial query by which we map the geojson representation of a table
-var geojsonMarkerOptions = {
-    radius: 6,
-    fillColor: "#ff7800",
-    color: "#FFF",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
-
-app.geojsonClient.execute("SELECT * FROM geom_data_js_v2") // 'LIMIT' should be added to the end of this line
-  .done(function(data) {
-    L.geoJson(data, {
-      onEachFeature: function(feature, layer) {
-        layer.on('click', function() {
-          layer.bindPopup(String("<b>Attack date:</b> " + feature.properties.iday + "/" + feature.properties.imonth + "/" + feature.properties.iyear + "<br><b>Perpetrating group:</b> "+feature.properties.gname + "<br><b>Location:</b> " + feature.properties.city+"<br><b>Tactic:</b> "+feature.properties.weaptype1_txt+"<br><b>Number Killed:</b> "+feature.properties.nkill)).openPopup();
-          fillForm(feature.properties);
-          console.log(feature.properties);
-        });
-      },
-      pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, geojsonMarkerOptions);
-      }
-    }).addTo(app.map);
-  })
-  .error(function(errors) {
-  });
-
-// Leaflet draw setup
-app.map.addLayer(app.drawnItems);
-
-// Initialise the draw control and pass it the FeatureGroup of editable layers
-app.map.addControl(
-  new L.Control.Draw({
-    edit: {
-      featureGroup: app.drawnItems
-    },
-    draw: {
-      rectangle: false,
-      polyline: false,
-      polygon: false,
-      marker: false,
-      circle: true
-    }
-  })
-);
-
-// Automatically fill the form on the left from geojson response
-var fillForm = function(properties) {
-  $('#cartodb_id').val(properties.cartodb_id);
-  $('#name').val(properties.name);
-};
-
-// Handling the creation of Leaflet.Draw layers
-// Note the use of drawnLayerID - this is the way you should approach remembering and removing layers
-var drawnLayerID;
-app.map.on('draw:created', function (e) {
-  var type = e.layerType;
-  var layer = e.layer;
-  console.log('draw created:', e);
+cartodb.createLayer(map, {
+  user_name: 'arobbins',
+  type: 'cartodb',
+  sublayers: [{
+   sql: 'select * from geom_data_js_v2',
+   cartocss: '#geom_data_js_v2 {marker-fill: #ff7800; }',
+   interactivity: 'iyear, imonth, iday, gname, city, weaptype1_txt, nkill'
+  }]
+})
+.addTo(map)
+.done(function(layer) {
+  cdb.vis.Vis.addInfowindow(map, layer.getSubLayer(0), ['iyear', 'imonth', 'iday', 'gname', 'city', 'weaptype1_txt', 'nkill']);
 });
 
 //Download CSV with the data on click
